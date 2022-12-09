af5d8f812320fba3ed32fc9e888fe4668228bf90
krisv <105808514+krisvers@users.noreply.github.com>
2022-12-09 15:57:01 -0500

    Update shellthon.py

--- shellthon.py
+++ shellthon.py
@@ -1,21 +1,62 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+import os, sys
 
-import os
-import sys
+version = "1.1.0"
 
-prevcmd = ""
+def get_prompt():
+    return str(os.environ.get('USER'))+" : "+str(os.environ.get('PWD').replace(os.environ.get('HOME'), "~"))+" -> "
 
-while (1):
-    print("shellthon :> ", end = "")
-    inp = input()
+def internal_cmd(cmd):
+    if not len(cmd) > 0:
+        return True
 
-    if inp == "exit":
+    argv = cmd.split()
+    argc = len(argv)
+    if (argv[0] == "version" or argv[0] == "ver"):
+        print("shellthon version: "+str(version))
+
+        return True
+    elif (argv[0] == "cd"):
+        if (argc == 2):
+            if (argv[1].startswith("~")):
+                try:
+                    os.chdir(argv[1].replace("~", os.environ.get('HOME')))
+                    os.environ['PWD'] = argv[1].replace("~", os.environ.get('HOME'))
+                except FileNotFoundError as e:
+                    print("cd: no such file or directory found")
+
+                return True
+
+            try:
+                os.chdir(argv[1])
+                os.environ['PWD'] = argv[1]
+            except FileNotFoundError as e:
+                print("cd: "+argv[1]+": no such file or directory found")
+        else:
+            try:
+                os.chdir(os.environ.get('HOME'))
+                os.environ['PWD'] = os.environ.get('HOME')
+            except FileNotFoundError as e:
+                print("cd: no home directory found")
+
+        return True
+    elif (argv[0] == "quit" or argv[0] == "exit"):
         sys.exit()
-        ref = False
-        break
-    elif inp == "prevcmd":
-        inp = last
     else:
-        prevcmd = inp
+        return False
 
-    print(os.system(inp))
+def main():
+    inp = str()
+
+    while (True):
+        print(get_prompt(), end="")
+        try:
+            inp = input()
+            if not internal_cmd(inp):
+                print(os.system(inp))
+        except EOFError as e:
+            print(e)
+        except KeyboardInterrupt as e:
+            print(e)
+
+main()
